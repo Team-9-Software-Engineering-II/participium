5266a8f150187f8df259021c18f47efb511fc528
ELENA LUBRANO s345376 <s345376@studenti.polito.it>
2025-11-18 22:42:42 +0100

    test: add e2e tests for login flow

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,20 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add(
+  "loginAsAdmin",
+  (username = "admin", password = "AdminPass123!") => {
+    cy.visit("/login");
+    cy.get("input[name=username]").type(username);
+    cy.get("input[name=password]").type(password);
+    cy.get("button[type=submit]").click();
+  }
+);
+
+Cypress.Commands.add("loginAsUser", (username, password) => {
+  cy.visit("/login");
+  cy.get("input[name=username]").type(username);
+  cy.get("input[name=password]").type(password);
+  cy.get("button[type=submit]").click();
+});
+
+Cypress.Commands.add("logout", () => {
+  cy.get("button#logout").click();
+});
