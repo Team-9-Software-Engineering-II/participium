e227032eaf60c53083ac32cb9d322fc8d473ef37
Giulia Bax <s345248@studenti.polito.it>
2025-11-25 18:23:06 +0100

    Remove unused acceptOrRejectReport function from municipal router

--- server/routers/municipal-router.mjs
+++ server/routers/municipal-router.mjs
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import {
-  acceptOrRejectReport,
   changeProblemCategory,
   getPendingApprovalReports,
   reviewReport,
@@ -23,7 +22,6 @@ router.put(
   "/reports/:reportId",
   isAuthenticated,
   isPublicRelationsOfficer,
-  acceptOrRejectReport
   reviewReport
 );
 
