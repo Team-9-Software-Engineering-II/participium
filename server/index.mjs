62a411a46bc030710d565208066735cb690fc9ef
raffaZunino <raffaele.zunino99@gmail.com>
2025-11-26 17:53:43 +0100

    updated index

--- server/index.mjs
+++ server/index.mjs
@@ -39,7 +39,7 @@ function bootstrapExpress() {
       saveUninitialized: false,
       cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: false, //process.env.NODE_ENV === "production",
         maxAge: 1000 * 60 * 60 * 24,
       },
     })
